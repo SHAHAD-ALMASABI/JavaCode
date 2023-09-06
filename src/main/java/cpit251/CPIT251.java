9bd9b5aaebdd0edea5eb1f6cfb80a7014106a78a
shahad Hamad Almasabi <sabdullahalmasabi0001@stu.kau.edu.sa>
2023-09-06 16:33:59 +0300

    first commit!

---
+++ src/main/java/cpit251/CPIT251.java
@@ -0,0 +1,14 @@
+/*
+ * SHAHAD HAMAD ALMASABI
+* ID - 2108845
+* AA8
+ */
+
+package cpit251;
+
+public class CPIT251 {
+
+    public static void main(String[] args) {
+        System.out.println ("hello world my name is shams");
+    }
+}
